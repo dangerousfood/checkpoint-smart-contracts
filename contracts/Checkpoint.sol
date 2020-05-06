fb509c24df678a87c19c66bf12e359275e2afa58
Joseph Delong <joseph@delong.me>
2020-05-05 19:45:46 -0500

    added test cases for Checkpoint.sol; Removed sparse merkle impl in favor of simplicity

--- contracts/Checkpoint.sol
+++ contracts/Checkpoint.sol
@@ -3,8 +3,20 @@ pragma solidity >=0.4.21 <0.7.0;
 contract Checkpoint {
   address public owner;
   mapping(address => bytes32) public currentMerkleRoot;
-  bytes32[32] zeroBranch;
-  uint TREE_DEPTH = 32;
+
+  event RootUpdated(
+      address sender,
+      bytes32 currentRoot,
+      bytes32 prevRoot,
+      uint timestamp
+  );
+
+  event HistoryRolledBack(
+      address sender,
+      bytes32 currentRoot,
+      bytes32 prevRoot,
+      uint timestamp
+  );
 
   struct RootData {
       //timestamp of current root
@@ -16,17 +28,14 @@ contract Checkpoint {
   mapping(address => mapping(bytes32 => RootData)) public history;
 
   constructor() public {
-    owner = msg.sender;
-    for (uint i = 0; i < TREE_DEPTH - 1; i++)
-        zeroBranch[i+1] = sha256(abi.encodePacked(zeroBranch[i], zeroBranch[i]));
-  }
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
   }
   
-  function initData() public view returns (bytes32[32] memory) {
-      return zeroBranch;
+  function getPreviousMerkleRoot(address account) public view returns(bytes32){
+      return history[account][currentMerkleRoot[account]].prevRoot;
+  }
+
+  function getCurrentMerkleRoot(address account) public view returns(bytes32){
+      return currentMerkleRoot[account];
   }
 
   function rollBackCurrentMerkleRoot(bytes32 root) public {
@@ -34,51 +43,20 @@ contract Checkpoint {
       require(history[msg.sender][root].timestamp != 0, 'Provided root does not exist in the history');
       currentMerkleRoot[msg.sender] = root;
       //emit history rolled back event
+      emit HistoryRolledBack(msg.sender, root, history[msg.sender][root].prevRoot, block.timestamp);
   }
 
   function updateCurrentMerkleRoot(bytes32 root) public {
       require(root != currentMerkleRoot[msg.sender], 'Root provided matches current merkle root');
-      //get previous root and timestamp (maybechange timestamp to )
+      //get previous root and timestamp (maybe change timestamp to block.root)
       Checkpoint.RootData memory currentRootData = RootData({
           timestamp: block.timestamp,
           prevRoot: currentMerkleRoot[msg.sender]
       });
       //update current merkle root
+      currentMerkleRoot[msg.sender] = root;
       history[msg.sender][root] = currentRootData;
       //emit root updated event
-  }
-  
-  function calculateBranchIndex(uint256 size) private pure returns(uint128 branchIndex) {
-      uint256 _size = size;
-      for (uint i = 0; i < 32; i++) {
-          if(size & 1 == 1) break;
-          branchIndex++;
-          _size /= 2;
-      }
-  }
-
-  function calculateRoot(bytes32[32] memory branch, uint size) public view returns(bytes32 root) {
-    uint _size = size;
-    root = 0;
-    for(uint h = 0; h < 32; h++){
-        if (size & 1 == 1) root = sha256(abi.encodePacked(branch[h], root));
-        else root = sha256(abi.encodePacked(root, zeroBranch[h]));
-        _size /= 2;
-    }
-  }
-
-  function calculateBranch(bytes32[32] memory branch, bytes32 data, uint size) public pure returns(bytes32[32] memory calculatedBranch) {
-      calculatedBranch = branch;
-      bytes32 _data = data;
-      uint128 branchIndex = calculateBranchIndex(size);
-      for(uint i = 0; i < 32; i++) {
-          if (i < branchIndex) _data = sha256(abi.encodePacked(branch[i], _data));
-          else break;
-          calculatedBranch[i] = _data;
-      }
-  }
-
-  function validateData(bytes32 root, bytes32[32] memory branch, bytes32 leaf, uint256 size) public pure returns(bool) {
-      return (root == calculateBranch(branch, leaf, size)[0]);
+      emit RootUpdated(msg.sender, root, currentRootData.prevRoot, currentRootData.timestamp);
   }
 }
